1642cb6ee9ec1ec6fcf82effe2e09e6292b50a66
Pond <pimpakan2545@gmail.com>
2025-03-26 14:44:51 +0700

    edit UI form booking room

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,12 @@
 module BookingsHelper
+  def time_options_for_select
+    options = []
+    (8..18).each do |hour|
+      ["00", "30"].each do |minute|
+        time = "#{hour.to_s.rjust(2, '0')}:#{minute}"
+        options << [time, time]
+      end
+    end
+    options
+  end
 end
