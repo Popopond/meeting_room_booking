1dda36c9dc9a3fc80f96849d20f20916f5ab56a5
Pond <pimpakan2545@gmail.com>
2025-04-08 13:26:03 +0700

    Update schema.rb to change the database version to 2025_03_26_044454 and add the "title" field in the bookings table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_081209) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_26_044454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_081209) do
     t.string "confirmation_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["room_id"], name: "index_bookings_on_room_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
