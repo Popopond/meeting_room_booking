4a20adbc12f9b394315dad78889fcdc75b4f4b1e
Pond <pimpakan2545@gmail.com>
2025-04-11 13:02:59 +0700

    Improved validation in the user model, added checks for password complexity and avatar size, and improved formatting and colors in HTML files to align with the new theme.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,37 @@ class User < ApplicationRecord
     has_many :meeting_participants, dependent: :destroy
     has_many :participated_bookings, through: :meeting_participants, source: :booking
 
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    has_one_attached :avatar
+
+    # Validations
+    validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false },
+                        length: { minimum: 3, maximum: 20 },
+                        format: { with: /\A[a-zA-Z0-9_]+\z/, message: "สามารถใช้ได้เฉพาะตัวอักษร ตัวเลข และ _ เท่านั้น" }
+    
+    validates :email, presence: true, 
+                     uniqueness: { case_sensitive: false },
+                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "รูปแบบอีเมลไม่ถูกต้อง" }
+    
+    validate :password_complexity
+    validate :avatar_size
 
     # Scopes
     scope :exclude_user, ->(user) { where.not(id: user.id) }
+
+    private
+    
+    def password_complexity
+      return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+      
+      errors.add :password, 'ต้องมีอย่างน้อย 8 ตัวอักษร ประกอบด้วยตัวอักษรและตัวเลขอย่างน้อย 1 ตัว'
+    end
+
+    def avatar_size
+      return unless avatar.attached?
+      
+      if avatar.blob.byte_size > 5.megabytes
+        errors.add(:avatar, 'ขนาดไฟล์ต้องไม่เกิน 5MB')
+      end
+    end
 end
