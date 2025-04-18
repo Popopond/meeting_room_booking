4ef2ed4d515d38be30820ed71ccf25caa2a1d2eb
Pond <pimpakan2545@gmail.com>
2025-04-19 00:49:19 +0700

    Update production environment configuration for ActionMailer to use new SMTP settings and host. Enhance security by utilizing environment variables for Gmail credentials.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,15 +58,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "meeting-room-booking-b3l0.onrender.com" }
 
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  # Specify outgoing SMTP server
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: Rails.application.credentials.dig(:smtp, :user_name),
-    password: Rails.application.credentials.dig(:smtp, :password),
     address: "smtp.gmail.com",
     port: 587,
-    authentication: :plain
+    domain: "gmail.com",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_APP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
